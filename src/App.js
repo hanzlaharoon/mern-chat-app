133e1230a17401a623606d47c563fc7e63c55882
Hanzla Haroon <hanzla.haroon@outlook.com>
2021-04-13 20:20:59 +0500

    Configured React Router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import logo from './logo.svg';
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "bootstrap/dist/css/bootstrap.css";
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./components/login";
+import Register from "./components/register";
 
 function App() {
-  return (
+  return (<Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+
+            <Navbar.Brand>
+              <Link to={"/login"} className="nav-link">
+                React MERN Stack Chat App
+              </Link>
+            </Navbar.Brand>
+
+            <Nav className="justify-content-end">
+              <Nav>
+                <Link to={"/login"} className="nav-link">
+                  Login
+                </Link>
+              </Nav>
+
+              <Nav>
+                <Link to={"/register"} className="nav-link">
+                  Register
+                </Link>
+              </Nav>
+            </Nav>
+
+            {/* <Nav>
+                <Link to={"/edit-student/:id"} className="nav-link">
+                  Edit Student
+                </Link>
+              </Nav> */}
+
+          </Container>
+        </Navbar>
       </header>
+
+      <Container>
+        <Row>
+          <Col md={12}>
+            <div className="wrapper">
+              <Switch>
+                <Route exact path='/' component={Login} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+                {/* <Route path="/edit-student/:id" component={EditStudent} /> */}
+              </Switch>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
-  );
+  </Router>);
 }
 
 export default App;
